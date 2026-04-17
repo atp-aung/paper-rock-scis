2ba0b162676abba7ef97b69e9bde1cc650937970
atp-aung <atp.aung.5555@gmail.com>
2026-04-17 19:47:35 +0630

    b

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function getComputerChoice() {
     return "scissors";
   }
 }
+
+test("getComputerChoice returns valid options", () => {
+  const choices = ["rock", "paper", "scissors"];
+  for (let i = 0; i < 100; i++) {
+    // Run many times to cover all branches
+    const result = getComputerChoice();
+    expect(choices).toContain(result);
+  }
+});
