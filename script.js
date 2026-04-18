9e1ffc7c63144526e0b05327bb1e627aeafc5eb4
atp-aung <atp.aung.5555@gmail.com>
2026-04-18 10:20:24 +0630

    bt

--- script.js
+++ script.js
@@ -9,4 +9,23 @@ const tess1 = function getComputerChoice() {
   }
 };
 
-module.exports = tess1;
+const tess2 = function getHumanChoice() {
+  const signButton = document.querySelector("#humanChooseButton");
+  const log = document.querySelector("#log");
+
+  signButton.addEventListener("click", () => {
+    let sign = prompt("What's your sign?");
+
+    if (sign === null) {
+      log.innerText = "OK, maybe next time.";
+    } else if (sign.toLowerCase() === "") {
+      log.innerText = "Don't be shy, enter your sign!";
+    } else if (sign.toLowerCase() === "scorpio") {
+      log.innerText = "Wow! I'm a Scorpio too!";
+    } else {
+      log.innerText = `${sign} is my favorite!`;
+    }
+  });
+};
+
+module.exports = { tess1, tess2 };
