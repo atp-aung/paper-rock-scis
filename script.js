b98d794f173d407681001b35e9faee0b40b82f5e
atp-aung <atp.aung.5555@gmail.com>
2026-04-18 12:07:04 +0630

    g

--- script.js
+++ script.js
@@ -1,48 +1,26 @@
-// const tess1 = function getComputerChoice() {
-//   const randomNumber = Math.random();
-//   if (randomNumber < 1 / 3) {
-//     return "rock";
-//   } else if (randomNumber < 2 / 3) {
-//     return "paper";
-//   } else {
-//     return "scissors";
-//   }
-// };
-
-// const tess2 = function getHumanChoice() {
-//   const signButton = document.querySelector("#humanChooseButton");
-//   const log = document.querySelector("#log");
-
-//   signButton.addEventListener("click", () => {
-//     let sign = prompt(`type (p,r,s)`);
-
-//     if (sign === null) {
-//       log.innerText = "OK, maybe next time.";
-//     } else if (sign.toLowerCase() === "") {
-//       log.innerText = "Don't be shy, enter your sign!";
-//     } else if (sign.toLowerCase() === "p") {
-//       log.innerText = "you choose paper";
-//     } else if (sign.toLowerCase() === "r") {
-//       log.innerText = "you choose rock";
-//     }
-//   });
-// };
-
-function getHumanChoice() {
-  // Select the button
-  const button = document.getElementById("humanChooseButton");
-
-  // Add click event
-  button.addEventListener("click", function () {
-    let userInput = prompt("Enter your name:");
-
-    if (userInput !== null && userInput !== "") {
-      alert("Hello, " + userInput + "!");
-    } else {
-      alert("You didn't enter anything.");
-    }
-  });
+const randomNumber = Math.random();
+if (randomNumber < 1 / 3) {
+  console.log(`computer choose rock and the value is ${randomNumber}`);
+} else if (randomNumber < 2 / 3) {
+  console.log(`computer choose paper and the value is ${randomNumber}`);
+} else {
+  console.log(`computer choose scissors and the value is ${randomNumber}`);
 }
 
-//module.exports = { tess1, tess2 };
-//module.exports = tess1;
+// Select the button
+const humanChoosebutton = document.getElementById("humanChooseButton");
+
+// Add click event
+humanChoosebutton.addEventListener("click", function () {
+  let userInput = prompt(`type (p,r,s)`);
+
+  if (userInput === null || userInput === "") {
+    console.log(`You didn't enter anything.`);
+  } else if (userInput.toLowerCase() === "r") {
+    console.log(`You choose rock`);
+  } else if (userInput.toLowerCase() === "p") {
+    console.log(`You choose paper`);
+  } else {
+    console.log(`type r,p,s`);
+  }
+});
