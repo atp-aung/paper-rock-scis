48ed9b4961607214045f01dd4c91e65c157544d6
atp-aung <atp.aung.5555@gmail.com>
2026-05-28 18:38:24 +0630

    g

--- script.js
+++ script.js
@@ -12,59 +12,30 @@ function getComputerChoice() {
   }
 }
 
-// function getHumanChoice() {
-//   //let userInput = prompt(`type rock, paper or scissors`);
+function playGame(ev) {
+  const computerChoice = getComputerChoice();
+  const humanChoice = ev.target.textContent.toLowerCase();
 
-//   if (userInput === "" || userInput === null) {
-//     console.log(`type something`);
-//     return "nothing";
-//   } else if (userInput.toLowerCase() === "rock") {
-//     return `rock`;
-//   } else if (userInput.toLowerCase() === "paper") {
-//     return `paper`;
-//   } else if (userInput.toLowerCase() === "scissors") {
-//     return `scissors`;
-//   } else {
-//     console.log(`invalid input`);
-//     return `invalid input`;
-//   }
-// }
+  console.log(`You: ${humanChoice}`);
+  console.log(`Computer: ${computerChoice}`);
 
-function getHumanChoice(ev) {
-  const humanChoice = ev.target.textContent;
-  if (humanChoice.toLowerCase() === "rock") {
-    console.log(humanChoice);
-    return `rock`;
-  } else if (humanChoice.toLowerCase() === "paper") {
-    console.log(humanChoice);
-    return `paper`;
-  } else {
-    console.log(humanChoice);
-    return `scissors`;
-  }
-}
-
-function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     console.log(`It's a tie!`);
-  } else if (humanChoice === "nothing" || humanChoice === "invalid input") {
-    console.log(`nothing or invalid`);
-    //return "invalid";
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
     console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    return "human";
   } else {
     console.log(`You lose!\n${computerChoice} beats ${humanChoice}`);
-    return "computer";
   }
+
+  console.log(`------------------------------`);
 }
 
 const btnRock = document.querySelector("#btnRock");
-btnRock.addEventListener("click", getHumanChoice);
+btnRock.addEventListener("click", playGame);
 
 btnRock.addEventListener("mouseover", () => {
   btnRock.style.backgroundColor = "black";
@@ -87,7 +58,7 @@ btnRock.addEventListener("mouseout", () => {
 // });
 
 const btnPaper = document.querySelector("#btnPaper");
-btnPaper.addEventListener("click", getHumanChoice);
+btnPaper.addEventListener("click", playGame);
 
 btnPaper.addEventListener("mouseover", () => {
   btnPaper.style.backgroundColor = "black";
@@ -100,7 +71,7 @@ btnPaper.addEventListener("mouseout", () => {
 });
 
 const btnScissors = document.querySelector("#btnScissors");
-btnScissors.addEventListener("click", getHumanChoice);
+btnScissors.addEventListener("click", playGame);
 
 btnScissors.addEventListener("mouseover", () => {
   btnScissors.style.backgroundColor = "black";
@@ -113,27 +84,20 @@ btnScissors.addEventListener("mouseout", () => {
 });
 
 // function playGame() {
-//   for (let i = 1; i <= 5; i++) {
-//     console.log(`--- Round ${i} ---`);
+//   console.log(`Human chose: ${humanChoice}`);
+//   console.log(`Computer chose: ${computerChoice}`);
 
-//     const humanChoice = getHumanChoice();
-//     const computerChoice = getComputerChoice();
+//   const result = playRound(humanChoice, computerChoice);
 
-//     console.log(`Human chose: ${humanChoice}`);
-//     console.log(`Computer chose: ${computerChoice}`);
-
-//     const result = playRound(humanChoice, computerChoice);
-
-//     if (result === "human") {
-//       humanScore++;
-//     } else if (result === "computer") {
-//       computerScore++;
-//     } else {
-//       console.log(`return ${result} only`);
-//     }
-
-//     console.log(`Score => Human: ${humanScore}, Computer: ${computerScore}`);
+//   if (result === "human") {
+//     humanScore++;
+//   } else if (result === "computer") {
+//     computerScore++;
+//   } else {
+//     console.log(`return ${result} only`);
 //   }
+
+//   console.log(`Score => Human: ${humanScore}, Computer: ${computerScore}`);
 
 //   console.log(`--- Final Result ---`);
 
